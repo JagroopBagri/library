f8ffc2fe4a724b9a1c25727c3e368b9108975734
Jagroop <jagroopbagri5@gmail.com>
2021-12-28 00:08:27 -0800

    Updating CSS and starting to work on functionality via JavaScript

--- main.js
+++ main.js
@@ -14,5 +14,33 @@ function Book(title, author, pages, status){
 };
 // Add books to array function
 function addBook(){
+    if(titleValue !== '' && authorValue !== '' && pagesValue !== '' && (yesValue === true || noValue === true)){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('books');
+        bookList.appendChild(newDiv);
+    }
+    else{
+        alert('Error: All fields must be completed.');
+    }
     
 };
+
+// Global Variables
+
+let addNewBookButton = document.querySelector('.addNewBookButton');
+let titleValue;
+let authorValue;
+let pagesValue; 
+let yesValue; 
+let noValue; 
+let bookList = document.querySelector('.bookList');
+// Event listener for when addNewBook button is clicked
+
+addNewBookButton.addEventListener('click', function(){
+    titleValue = document.querySelector('#title').value;
+    authorValue = document.querySelector('#author').value;
+    pagesValue = document.querySelector('#pages').value;
+    yesValue = document.querySelector('#yes').checked;
+    noValue = document.querySelector('#no').checked;
+    return addBook();
+});
