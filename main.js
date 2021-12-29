49721fdfeefbeaa9908aee6a68f0d1e3d89d134a
Jagroop <jagroopbagri5@gmail.com>
2021-12-29 00:50:59 -0800

    Working on functionality

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ function displayLibrary(array){
         if(document.getElementById(element.id) !== null){
             return;
         }
+        // creating and appending elements to display new book
         let containerDiv = document.createElement('div');
         containerDiv.classList.add('books');
         containerDiv.id = element.id;
@@ -48,7 +49,7 @@ function displayLibrary(array){
         pagesDiv.textContent = element.pages;
         containerDiv.appendChild(pagesDiv);
         let statusDiv = document.createElement('div');
-        (yesValue === true) ? statusDiv.classList.add('read') : statusDiv.classList.add('unread'); // add class to read and unread text
+        (yesValue === true) ? statusDiv.classList.add('read') : statusDiv.classList.add('unread'); // add unique classes to read and unread text
         statusDiv.textContent = element.status;
         containerDiv.appendChild(statusDiv);
         let editDeleteDiv = document.createElement('div');
@@ -64,12 +65,19 @@ function displayLibrary(array){
         deleteButton.type = 'button';
         deleteButton.textContent = 'Delete';
         editDeleteDiv.appendChild(deleteButton);
+        deleteButtons = document.querySelectorAll('.delete'); // individual delete buttons
+        deleteButtons.forEach(function(deletebtn){
+            deletebtn.addEventListener('click', function(){
+                console.log('hello');
+            });
+        });
     });
 };
-
 // Global Variables
 let bookNumber = 0; // keep track of book number
 let addNewBookButton = document.querySelector('.addNewBookButton'); // add new book button
+let editButtons; // edit buttons that are created when books are created
+let deleteButtons; // delete buttons that are created when books are created
 let titleValue; // title of new book being added
 let authorValue; // author of new book being added
 let pagesValue; // pages of new book being added
@@ -77,6 +85,7 @@ let yesValue; // yes button boolean
 let noValue; // no button boolean
 let readStatus; // matches boolean value of yes button
 let bookList = document.querySelector('.bookList'); 
+
 // Event listener for when addNewBook button is clicked
 
 addNewBookButton.addEventListener('click', function(){
@@ -88,3 +97,19 @@ addNewBookButton.addEventListener('click', function(){
     addBook();
     displayLibrary(myLibrary);
 });
+
+// Event listener for when delete buttons are clicked
+
+if(deleteButtons !== undefined){
+    deleteButtons.forEach(function(deletebtn){
+        deletebtn.addEventListener('click', function(){
+            myLibrary.forEach(function(book){
+                console.log(book.id);
+                console.log(deleteButtons.indexOf(deletebtn));
+                    if(book.id === deleteButtons.indexOf(deletebtn)){
+                            return console.log('it works');
+                    };
+            })
+        });
+    });
+};
